03fc78cc6fe69f9b781dd3b73007b9fe5df71d8b
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-11-18 14:15:35 -0300

    Update paciente.py

--- paciente.py
+++ paciente.py
@@ -7,17 +7,27 @@ class Patient():
     address=''              ##direccion  
     bloody_group=''         ##grupo sanguineo
     medical_history=''      ##historial medico
+    phone=''                ##telefono o celular
+    allergy=''              ##alergia
+    detected=''             ##detectada
+    allergic=''             ##alergico
+    affiction=''            ##enfermedad
     ##Fin atributos##
 
     ##metodos##
-    def __init__(self,name,last,birthday,run,address,bloody_group,past):
+    def __init__(self,name,last,birthday,run,address,bloody_group,phone,allergy,allergic,past,detected,affiction):
         self.name_patient=name
         self.last_name=last
         self.date_of_birthday=birthday
         self.run=run
         self.address=address
         self.bloody_group=bloody_group
+        self.allergy=allergy
+        self.allergic=allergic
+        self.phone=phone
         self.medical_history=past
+        self.detected=detected
+        self.affiction=affiction
 
     def imprimir(self):
         print(self.name_patient)
@@ -26,14 +36,44 @@ class Patient():
         print(self.run)
         print(self.address)
         print(self.bloody_group)
+        print(self.allergy)
+        print(self.phone)
         print(self.medical_history)
+        print(self.detected)
+        print(self.affiction)
     ##Fin Metodos##
 
 print("Ingrese los Datos del Paciente: ")
 name = input("Nombre: ")
 last = input("Apellido: ")
 birthday = input("Fecha de Nacimiento: ")
-run = input("Run: ")
+run = input("Run (No punto ni guión): ")
 address = input("Domicilio: ")
 bloody_group = input("Grupo sanguineo: ")
-past = input("Enfermedad prescrita: ")
+phone = input("Ingrese su numero de telefono o celular: ")
+allergy = input("¿Tiene alguna alergía (Si o No)?: ")
+if allergy=="Si" or allergy=="si":
+    allergic = input("¿A que tiene alergía?: ")
+    past = input("Enfermedad prescrita (Si o No): ")
+    if past=="si" or past=="Si":
+        affiction = input("¿Que enfermedad Tiene?: ")
+        detected = input("¿Desde hace cuanto? (expresar en meses): ")
+        print("Ficha de: ")
+        a=Patient(name,last,birthday,run,address,bloody_group,allergy,allergic,phone,past,affiction,detected)
+        a.imprimir()
+    else:
+        print("Ficha de: ")
+        a=Patient(name,last,birthday,run,address,bloody_group,allergy,allergic,phone,past)
+        a.imprimir()
+else:
+    past = input("Enfermedad prescrita (Si o No): ")
+    if past=="si" or past=="Si":
+        affiction = input("¿Que enfermedad Tiene?: ")
+        detected = input("¿Desde hace cuanto? (expresar en meses): ")
+        print("Ficha de: ")
+        a=Patient(name,last,birthday,run,address,bloody_group,allergy,phone,past,affiction,detected)
+        a.imprimir()
+    else:
+        print("Ficha de: ")
+        a=Patient(name,last,birthday,run,address,bloody_group,phone,allergy="No",allergic="No",past="No",affiction="No",detected="No")
+        a.imprimir()
